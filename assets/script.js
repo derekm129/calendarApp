d4b3b2ed00762029fd7e6413833224e4de9b0cf4
dmod129 <derek.modugno@gmail.com>
2023-09-06 19:21:36 -0500

    added compareTime function

--- assets/script.js
+++ assets/script.js
@@ -9,15 +9,39 @@ const ids = [
   'hour-10', 
   'hour-11', 
   'hour-12', 
-  'hour-1', 
-  'hour-2', 
-  'hour-3', 
-  'hour-4', 
-  'hour-5'];
+  'hour-13', 
+  'hour-14', 
+  'hour-15', 
+  'hour-16', 
+  'hour-17'];
 
 currentDay.innerHTML = date;
 
-$(function () {
+// Compare time function
+$(function() {
+  compareTime()
+});
+
+function compareTime() {
+  for (let i = 0; i < ids.length; i++) {
+    // Retrieve html element by id
+    const element = document.getElementById(ids[i]);
+    // Hour value from element id
+    const hourValue = parseInt(element.id.split('-')[1]);
+
+    if (hourValue === currentHour) {
+      element.classList.add('present');
+    }
+    else if (hourValue < currentHour) {
+      element.classList.add('past');
+    }
+    else if (hourValue > currentHour) {
+      element.classList.add('future');
+    }
+  }
+  };
+
+// $(function () {
 
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
@@ -25,18 +49,10 @@ $(function () {
   // function? How can DOM traversal be used to get the "hour-x" id of the
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  
-
 
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
-});
+ 
+
+compareTime();
